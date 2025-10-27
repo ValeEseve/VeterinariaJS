e4b39125cc92ea60b5aadae580762e4844a3e74a
Vale Eseve <vale.eseve@gmail.com>
2025-10-26 22:32:46 -0300

    Últimos ajustes

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-import { registrar, leerRegistros } from "./operaciones";
+import { registrar, leerRegistros, mostrarLista } from "./operaciones.js";
+
+const arg = process.argv.slice(2);
+
+(async function main() {
+  switch (arg[0]) {
+    case "registrar":
+      await registrar(arg);
+      break;
+    case "leer":
+      await mostrarLista();
+      break;
+    default:
+      console.log("Opción no válida");
+  }
+})();
--- operaciones.js
+++ operaciones.js
@@ -3,22 +3,42 @@ import { readFileSync, writeFileSync } from "node:fs";
 const leerRegistros = async () => {
   try {
     const data = readFileSync("citas.json", "utf-8");
-    console.log(data);
-    return JSON.parse(data);
+    return data;
   } catch (error) {
     console.log("Error leyendo las citas: " + error);
+    return [];
   }
 };
+
+const mostrarLista = async () => {
+  try {
+    const data = await leerRegistros()
+    JSON.parse(data).forEach((cita) => {
+      console.log(`
+        ------------------------------
+          Nombre: ${cita.nombre}
+          Edad: ${cita.edad}
+          Animal: ${cita.animal}
+          Color: ${cita.color}
+          Enfermedad: ${cita.enfermedad}
+        ------------------------------
+        `);
+    });
+  } catch (error) {
+    console.log("Error leyendo las citas: " + error);
+    return [];
+  }
+}
 
 const registrar = async (datos) => {
   try {
     const data = await leerRegistros();
     data.push({
-      nombre: datos[0],
-      edad: datos[1],
-      animal: datos[2],
-      color: datos[3],
-      enfermedad: datos[4],
+      nombre: datos[1],
+      edad: datos[2],
+      animal: datos[3],
+      color: datos[4],
+      enfermedad: datos[5],
     });
     writeFileSync("citas.json", JSON.stringify(data));
   } catch (error) {
@@ -28,6 +48,4 @@ const registrar = async (datos) => {
   }
 };
 
-leerRegistros();
-
-export { registrar, leerRegistros };
+export { registrar, leerRegistros, mostrarLista };
